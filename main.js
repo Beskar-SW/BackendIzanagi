e616d8316d0980433bf60f56b3f76e5eb9d4ec77
FrancoRogel_Johan <johan.franco147@gmail.com>
2022-06-01 03:01:10 -0500

    2

--- main.js
+++ main.js
@@ -125,7 +125,7 @@ app.delete("/Admin/delete/:id", (req, res) => {
   });
 });
 
-app.put("/Admin/update/:id", upload.fields([]),(req, res) => {
+app.put("/Admin/update/:id", upload.any(),(req, res) => {
   var id = req.params.id;
   var data = req.body;
   // var producto = data.producto;
